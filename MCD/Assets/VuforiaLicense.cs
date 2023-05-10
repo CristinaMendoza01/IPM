39eecc15469624c1d9bc7aede7850ae3adbaf98f
cmendoza <72572198+CristinaMendoza01@users.noreply.github.com>
2023-05-10 16:35:31 +0200

    1st ex commit

---
+++ MCD/Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVdEekdXdi8vLy8vQUFBQm1XdE1VejFBSjBhY254VlFPOElpWUphTEVieUxIUldWVDN5WXBmNmpqSU9MaWJkSVlVaXh1ZFRaNlRIbDlEUHFPbDk0dml0b1docGx2L1lCRVVoazJ4aW5lWnZwRzVNRXNsbWs3M1pDeUQ0MjIzcytuaDAvWEVnTFpKOHQ1cEMzR2xSYUI0OEN0N0wzL2VHbzJiWmU5MkhWWUlSaVRGTVRRSytEZHIyMW4vLy9jQy93cXF0VTB1MzhMYzM3TFUvLzlqZC9UalZ4ZnIzL2ozc29OVEErSm5WVkdJUXd3NlMrR3ptcHV6dlBqYkJSL3dvOUJ2Vk9aRjBwYXpYcGZPaFBZZlhxOEpaazU2d0ZjOXQyWHF5N0Zrb3E1Wk0ydnh5YWRRUDNqaG5BRXMrbVg3SG9Pdy9vKzhLV2N4a0pUY2ZLSmpVRlQ0Q0ZKR2lmS0VFUW5iVEZvd1dFRXFRY2xFQVR3TURhMGtUYkxIUjI=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
